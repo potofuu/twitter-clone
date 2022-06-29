a6ca6321c4e70695f903a1933ee66fa4f20081ae
Duan, Daniel Lee <dduan6@gatech.edu>
2022-06-29 03:44:32 -0400

    fixed logo

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,7 +7,7 @@ export default function Sidebar() {
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
         {/* Logo */}
         <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1 rounded-full">
-            <Image width="50" height="50" src="https://seeklogo.com/images/T/twitter-2012-positive-logo-916EDF1309-seeklogo.com.png"></Image>
+            <Image width="50" height="50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png"></Image>
         </div>
 
         {/* Menu */}
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["seeklogo.com"]
+    domains: ["upload.wikimedia.org"]
   }
 }
 
