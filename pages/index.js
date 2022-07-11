30c63d56debb1f473767b64742b70604749ae165
Duan, Daniel Lee <dduan6@gatech.edu>
2022-07-10 23:53:34 -0400

    added random user section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
 
-export default function Home({newsResults}) {
+export default function Home({newsResults, randomUsersResults}) {
   return (
     <div>
       <Head>
@@ -20,7 +20,7 @@ export default function Home({newsResults}) {
         <Feed/>
 
         {/* Widgets */}
-        <Widgets newsResults={newsResults.articles} />
+        <Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults.results} />
       </main>
     </div>
   )
@@ -32,11 +32,14 @@ export default function Home({newsResults}) {
 
 export async function getServerSideProps() {
   const newsResults = await fetch ("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json")
-  .then((res)=>res.json());
+  .then((res) => res.json());
+
+  const randomUsersResults = await fetch("https://randomuser.me/api/?results=30&inc=name,login,picture")
+  .then((res) => res.json());
 
   return {
     props: {
-      newsResults,
+      newsResults, randomUsersResults
     },
   };
 }
